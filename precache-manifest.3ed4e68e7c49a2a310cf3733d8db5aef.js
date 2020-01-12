4a4f8e006ef16c70c48927666329da394e1e3bec
jamielhf <18825046356@163.com>
2020-01-12 08:19:39 +0000

    Deploying to gh-pages from master 604d60ae3c605d9f3d7a5d1cf14a0321aeb897a0

---
+++ precache-manifest.3ed4e68e7c49a2a310cf3733d8db5aef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8006580a61e1c3ae42c3711be9ad9001",
+    "url": "/nest-font/index.html"
+  },
+  {
+    "revision": "e4fddf33163b358d5b33",
+    "url": "/nest-font/static/css/2.7ececfe9.chunk.css"
+  },
+  {
+    "revision": "acdd4c256d8eeac03f18",
+    "url": "/nest-font/static/css/main.499e2a1d.chunk.css"
+  },
+  {
+    "revision": "e4fddf33163b358d5b33",
+    "url": "/nest-font/static/js/2.898f8c3b.chunk.js"
+  },
+  {
+    "revision": "acdd4c256d8eeac03f18",
+    "url": "/nest-font/static/js/main.a91e04ca.chunk.js"
+  },
+  {
+    "revision": "77601b2f60d35db9745e",
+    "url": "/nest-font/static/js/runtime-main.c14a1196.js"
+  }
+]);
